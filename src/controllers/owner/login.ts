2903056ac29baab08448a94ed00fab7ccba9e7ea
Mason3144 <lts890303@gmail.com>
2022-11-20 14:53:36 +0900

    JWT를 이용하여 로그인API 완성

---
+++ src/controllers/owner/login.ts
@@ -0,0 +1,41 @@
+const db = require('../../db')
+import bcrypt from "bcrypt"
+import * as jwt from "jsonwebtoken";
+
+
+
+export const login = async (req, res) => {
+    try {
+        const { body: { username, password } } = req
+    const user = await db.findSingleUser(username, "username")
+    const checkPassword = await bcrypt.compare(password, user.password)
+    if (!user) {
+        res.send({ok:false,error:"유저가 존재하지 않습니다."})
+    } else if (!checkPassword) {
+        res.send({ok:false, error:"password가 일치하지 않습니다."})
+    }
+        const token = await jwt.sign({ id: user.id }, process.env.TOKEN_KEY);
+        
+    res.send({ ok: true, token })
+        
+        
+    } catch (error) {
+        res.send({ok:false, error})
+    }
+    
+    
+    
+    
+}
+
+
+// const user = await client.user.findFirst({ where: { username } });
+// if (!user) {
+//   return { ok: false, error: "User not found." };
+// }
+// const checkPassword = await bcrypt.compare(password, user.password);
+// if (!checkPassword) {
+//   return { ok: false, error: "Incorrect password." };
+// }
+// const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+// return { ok: true, token };
--- src/routers/rootRouter.ts
+++ src/routers/rootRouter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {  getHome, postSignup } from "../controllers/owner/createAccount";
+import { login } from "../controllers/owner/login";
 import { postFindAccount } from "../controllers/owner/postFindAccount";
 
 const rootRouter = require("express-promise-router")();
@@ -7,5 +8,7 @@ const rootRouter = require("express-promise-router")();
 rootRouter.get("/", getHome);
 rootRouter.post("/signup", postSignup);
 rootRouter.post("/find-account", postFindAccount)
+rootRouter.post("/login",login)
+
 
 export default rootRouter;
